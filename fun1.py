b3044a267ed7e4555e73cabcf7f8458654dbe376
shellyw19-meet <shellyw19@meet.mit.edu>
2017-07-27 12:54:10 +0200

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,11 @@
+
+
+
+def add_numbers(start, end):
+    c = 0
+    for number in range(start, end+1):
+        print(number)
+        c = c + number
+    return c
+answer = add_numbers(333,777)
+print(answer)
---
+++ fun2.py
@@ -0,0 +1,7 @@
+def draw_1d():
+    n = int(input("how many stars?"))
+    print("*"* n)
+    
+
+def draw_2d():
+    sing = input("Which sign do you want?")
