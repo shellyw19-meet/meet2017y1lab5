850fa76a8741c023254bf7b05131002b7c34ba96
shellyw19-meet <shellyw19@meet.mit.edu>
2017-07-30 11:48:35 +0300

    Stopping here

--- fun2.py
+++ fun2.py
@@ -1,7 +1,27 @@
-def draw_1d():
+def draw_1d_old():
     n = int(input("how many stars?"))
     print("*"* n)
     
 
-def draw_2d():
-    sing = input("Which sign do you want?")
+def draw_1d():
+    sign = input("Which sign do you want?")
+    times = int(input("How many times?"))
+    print(sign*times)
+
+
+def draw_1d_2(nu):
+    sign = str(input("Which sign do you want?"))
+    print(sign*nu)
+
+def draw_2d(b , c ):
+    sign = input("Which sign do you want?")
+    for b in range(b):
+        print(c*sign)
+
+def special_draw_2d(n , m , border , fill):
+    print(m*border)
+    for n in range(n - 2):
+        print(border + fill*(m-2) + border)
+    print(m*border)
+   
+      
